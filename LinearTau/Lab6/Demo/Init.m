8992bf8aceb21a13abce95ebd9ce2dcde83d1c77
KozlovMA <KozlovMA@kamaz.ru>
2023-11-14 09:45:35 +0300

    feature-Lab_6

---
+++ LinearTau/Lab6/Demo/Init.m
@@ -0,0 +1,7 @@
+clc
+clear all
+
+addpath("models");      % добавляем папку с моделями в PATH
+addpath("data");        % добавляем папку с данными в PATH
+
+Params                  % запускаем файл с параметрами
